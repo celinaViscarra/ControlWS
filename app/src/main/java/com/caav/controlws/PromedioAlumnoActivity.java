fb570a0ecb5e44ba19bc298e97d4811eb1bfb72a
celinaViscarra <xelaju_celina@yahoo.com>
2020-06-30 22:21:33 -0600

    promedio alumno java class

--- app/src/main/java/com/caav/controlws/PromedioAlumnoActivity.java
+++ app/src/main/java/com/caav/controlws/PromedioAlumnoActivity.java
@@ -2,13 +2,35 @@ package com.caav.controlws;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+@SuppressLint("NewApi")
 public class PromedioAlumnoActivity extends AppCompatActivity {
+    EditText carnetTxt;
+    TextView notaPromedioTxt;
+    //URL.
+    private final String urlLocal ="http://192.168.1.8/av12013/ws_db_carnet_group.php";
 
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promedio_alumno);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        carnetTxt = (EditText) findViewById(R.id.editText_alumnoCarnet);
+        notaPromedioTxt = (TextView) findViewById(R.id.textView_alumnoNota);
+    }
+
+    public void consultarPromedio(View v){
+        String carnet = carnetTxt.getText().toString();
+        String url = urlLocal + "?carnet=" + carnet;
+        String notaPromedio = ControladorServicio.obtenerPromedioJSON(url, this);
+        notaPromedioTxt.setText("Nota Promedio: " + ControladorServicio.obtenerPromedioJSON(notaPromedio, this));
     }
 }
