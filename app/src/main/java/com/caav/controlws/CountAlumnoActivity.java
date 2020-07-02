bdc6e45f39a7d63dc830d8e6fb0bcc76db322519
celinaViscarra <xelaju_celina@yahoo.com>
2020-07-01 22:30:09 -0600

    especiales. part 1

---
+++ app/src/main/java/com/caav/controlws/CountAlumnoActivity.java
@@ -0,0 +1,14 @@
+package com.caav.controlws;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CountAlumnoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count_alumno);
+    }
+}
---
+++ app/src/main/java/com/caav/controlws/MaximoAlumnoActivity.java
@@ -0,0 +1,14 @@
+package com.caav.controlws;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaximoAlumnoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maximo_alumno);
+    }
+}
