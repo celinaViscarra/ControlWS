86715ce07805e5427829dbd113ea65fb8097747f
celinaViscarra <xelaju_celina@yahoo.com>
2020-07-01 19:50:01 -0600

    eliminar nota.

---
+++ app/src/main/java/com/caav/controlws/EliminarNotaActivity.java
@@ -0,0 +1,14 @@
+package com.caav.controlws;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EliminarNotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eliminar_nota);
+    }
+}
